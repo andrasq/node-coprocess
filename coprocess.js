81a6b473735ca56d836818889af18e76e207714d
Andras <andras@andrasq.com>
2022-03-20 18:21:55 -0400

    version of 12/22/21

--- coprocess.js
+++ coprocess.js
@@ -2,7 +2,7 @@ var cp = require('child_process');
 var fs = require('fs');
 
 var qibl = require('qibl');
-var qbson = require('qbson');
+//var qbson = require('qbson');
 
 module.exports.WorkerProcess = WorkerProcess;
 
@@ -35,11 +35,11 @@ function WorkerProcess( ) {
     this.process = function process_( calls ) {
         if (!calls || !(calls instanceof Object)) throw new Error('calls must be a hash');
         for (var name in calls) this.calls[name] = calls[name];
-        process.on('message', this._runCall);
+        process.on('message', this._handleCall);
     }
     this.close = function close( ) {
         if (this.child) this.child.connected && this.child.disconnect();
-        else process.disconnect && (process.disconnect(), process.removeListener('message', this._runCall));
+        else process.disconnect && (process.disconnect(), process.removeListener('message', this._handleCall));
     }
 
     this.callCount = 0;
@@ -49,20 +49,25 @@ function WorkerProcess( ) {
         if (++this.callCount & 0xFFF === 0) this.callbacks = qibl.omitUndefined(this.callbacks);
         cb && cb(msg.err ? qibl.objectToError(msg.err) : msg.err, msg.value);
     }
-    this._runCall = function _runCall( msg ) {
-        msg && self.calls[msg.name] && self.calls[msg.name](msg.value, function(err, res) {
-            // TODO: return non-object errors as-is
-           self._sendTo(process, { id: msg.id, name: msg.name, value: res, err: err && qibl.errorToObject(err) });
-        })
+    this._handleCall = function _handleCall( msg ) {
+        var socket = msg === 'socket' && arguments[1], func = self.calls[msg.name];
+        func && func(msg.value, runCallback);
+        // func && (msg.argv ? (msg.argv.push(runCallback), qibl.invoke(func, msg.argv)) : func(msg.value, runCallback));
+        function runCallback(err, res) {
+            self._sendTo(process,
+                err ? { id: msg.id, err: qibl.errorToObject(err) } : { id: msg.id, value: res });
+            // self._sendTo(process, { id: msg.id, value: res, err: err && qibl.errorToObject(err) });
+        }
     }
     this._sendTo = function _sendTo( target, msg ) {
         // EPIPE is returned to the send() callback, but ERR_IPC_CHANNEL_CLOSED always throws
         // some node versions delay the 'disconnect' event, be sure to call back just once
-        try { target.send(msg, null, _onSendError) } catch (err) { _onSendError(err) }
-        function _onSendError(err) {
-            err && (err = (!target) ? new Error('not forked yet')
-                : (!target.send && !target.connected) ? new Error('not connected') : err);
-            err && self._invokeCallback({ id: msg.id, err: err });
+        try { target.send(msg, null, this._onSendError) } catch (err) { this._onSendError(err) }
+    }
+    this._onSendError = function _onSendError(err) {
+        if (err) { err = (!target) ? new Error('not forked yet')
+            : (!target.send && !target.connected) ? new Error('not connected') : err;
+            self._invokeCallback({ id: msg.id, err: err });
         }
     }
 }
@@ -95,7 +100,7 @@ console.log("AR: master");
                 console.log("AR: Done.");
             }
         )
-        // up to 238k/s concurrent calls
+        // up to 260k/s concurrent calls
     }
     function testSeries() {
         qibl.repeatFor(
@@ -108,7 +113,7 @@ console.log("AR: master");
                 wp.close();
             }
         )
-        // up to 83k/s back-to-back calls
+        // up to 85k/s back-to-back calls
     }
 }
 else {
